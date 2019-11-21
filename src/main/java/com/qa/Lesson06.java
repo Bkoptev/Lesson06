14cbfbab59f6325042ff376b491de558e7988803
bkoptev <hate1smiley@gmail.com>
2019-11-21 12:59:56 +0200

    final

--- src/main/java/com/qa/Lesson06.java
+++ src/main/java/com/qa/Lesson06.java
@@ -10,7 +10,6 @@ import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 
-import java.sql.Driver;
 import java.util.Collections;
 import java.util.concurrent.TimeUnit;
 
@@ -18,7 +17,10 @@ public class Lesson06 {
 
     // Instance of WebDriver
     private WebDriver driver;
-
+    private final String XPATH_CROSSED = "//span[contains(text(),'Samsung Galaxy S6')]/ancestor::" +
+            "div[@data-el='table_col']//p/span[contains(@data-mce-style,'line-through')]";
+    private final String XPATH = "//span[contains(text(),'Samsung Galaxy S6')]/ancestor::" +
+            "div[@class='ptsTableElementContent ptsElArea']//p/span[contains(@data-mce-style,'21pt;')]";
     /**
      * Set up method
      */
@@ -46,19 +48,23 @@ public class Lesson06 {
     public void testPriceComparision() {
         WebDriverWait wait = new WebDriverWait(driver, 10);
         // Test for   https://supsystic.com/example/comparison-example/
+
         driver.get("https://supsystic.com/example/comparison-example/");
-        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//div[contains(@class,'ptsCol-2')]//span[contains(text(),'959')]"))));
-        String price1 = driver.findElement(By.xpath("//div[contains(@class,'ptsCol-2')]//span[contains(text(),'959')]")).
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath(XPATH))));
+        String price1 = driver.findElement(By.xpath(XPATH)).
                 getText().replaceAll("^.*?(-?\\d+(\\.\\d+)?).*$", "$1");
-        String price2 = driver.findElement(By.xpath("//div[contains(@class,'ptsCol-2')]//span[contains(text(),'2699')]")).
-                getText().replaceAll("^.*?(-?\\d+(\\.\\d+)?).*$", "$1");;
+        String price2 = driver.findElement(By.xpath(XPATH_CROSSED)).
+                getText().replaceAll("^.*?(-?\\d+(\\.\\d+)?).*$", "$1");
+
         System.out.println("Samsung Galaxy S6  " + "$" + price1);
         System.out.println("Samsung Galaxy S6 crossed " + "$" + price2);
         float result1 = Float.parseFloat(price1);
         float result2 = Float.parseFloat(price2);
         System.out.println("Difference = " + (result2 - result1));
         // Test for   https://unicode-table.com/ru/
+
         driver.get("https://unicode-table.com/ru/");
+        wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.xpath("//li[@data-code='81']"))));
         System.out.println(driver.findElement(By.xpath("//li[@data-code='81']")).getText());
         System.out.println(driver.findElement(By.xpath("//li[@data-code='36']")).getText());
         System.out.println(driver.findElement(By.xpath("//li[@data-code='65']")).getText());
@@ -70,8 +76,6 @@ public class Lesson06 {
      */
     @After
     public void tearDown() {
-        // Quit from Driver. close() just close window,
-        // quit() - close all window an driver
         driver.quit();
     }
 }
